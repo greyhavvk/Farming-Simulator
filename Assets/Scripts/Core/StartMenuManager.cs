efc664167b2abc33f52455221cfc819c0de97025
Abdulkadir Semiz <abdulkadirsemiz@outlook.com>
2024-07-17 12:28:48 +0300

    add startMenu scene and add startMenuManager

---
+++ Assets/Scripts/Core/StartMenuManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Core
+{
+    public class StartMenuManager : MonoBehaviour
+    {
+        private const int GameSceneIndex = 1;
+
+        public void OnStartMenuButtonClicked()
+        {
+            SceneManager.LoadScene(GameSceneIndex);
+        }
+    }
+}
